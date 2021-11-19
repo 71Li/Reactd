97d9af2f24cf4cf90e2c6284772072b2962943cf
lian7171 <2638250647@qq.com>
2021-11-19 23:46:36 +0800

    Menu组件修改

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 // import Alert, {AltMessage, AltType} from "./component/Alert/alert";
 import Alert, {AltType} from "./component/Alert/alert";
+import Menu from "./component/Menu/menu";
+import MenuItem from "./component/Menu/menuItem";
 function App() {
     // @ts-ignore
     return (
@@ -11,9 +13,16 @@ function App() {
               {/*<Button size={ButtonSize.Large} btnType={ButtonType.Link}  target={"_blank"} href="http://www.baidu.com"> {"link"}</Button>*/}
               {/*<br />*/}
               {/*<Button size={ButtonSize.Small} btnType={ButtonType.Danger}> {"small"}</Button>*/}
-            <Alert message={"这是标题1"} altType={AltType.Success} show={false}> 这是详细内容</Alert>
+            {/*<Alert message={"这是标题1"} altType={AltType.Success} show={false}> 这是详细内容</Alert>*/}
 
-            <Alert message={"这是标题2"} altType={AltType.Info}> 这是详细内容</Alert>
+            {/*<Alert message={"这是标题2"} altType={AltType.Info}> 这是详细内容</Alert>*/}
+                <Menu mode={"vertical"} defaultIndex={0} onSelect={(index)=>{alert(index)}}>
+                    <MenuItem index={1}>列表项1</MenuItem>
+                    <MenuItem index={2} disabled>列表项2</MenuItem>
+                    <MenuItem index={3}>列表项3</MenuItem>
+
+                </Menu>
+
           </header>
         </div>
   );
--- src/component/Menu/menu.tsx
+++ src/component/Menu/menu.tsx
@@ -1,27 +1,50 @@
-import React, {ReactNode} from "react";
+import React, {ReactNode,useState,createContext} from "react";
 import classNames from 'classnames'
+import MenuItemProps from "./menuItem";
 
 
 type MenuMode = 'horizontal'|'vertical'
+type SelectCallback = (selectedIndex:number)=>void
 export interface MenuProps {
     defaultIndex ?: number,
     className ?: string,
     style ?: React.CSSProperties,
     mode ?:MenuMode,
-    onSelect ?: (selectedIndex:number)=>void,
+    onSelect ?:SelectCallback,
     children ?: ReactNode
 }
 
+interface IMenuContext {
+    index : number,
+    onSelect ?:SelectCallback,
+}
+
+export const MenuContext = createContext<IMenuContext>({index:0});
 
 const Menu:React.FC<MenuProps> =  (props) => {
-
-    const {defaultIndex, className, style, mode,children} = props
+    const {defaultIndex, className, style, mode,children,onSelect} = props
+    const [currentActive, setActive] = useState(defaultIndex);
     const classes = classNames('menu',className,{
         'menu-vertical': mode === 'vertical'
     })
 
+    const handleClick = (index:number)=>{
+        setActive(index);
+        if(onSelect){
+            onSelect(index)
+        }
+
+    }
+    const passedContext:IMenuContext = {
+        index: currentActive ? currentActive :0,
+        onSelect :handleClick
+    }
     return (
-        <ul className={classes} style={style}>{children}</ul>
+        <ul  className={classes} style={style} data-testid={'test-menu'}>
+            <MenuContext.Provider value={passedContext}>
+                {children}
+            </MenuContext.Provider>
+        </ul>
 
     )
 
